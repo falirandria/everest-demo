2361ca7d048a55a5ea4103a6be576efd45b870a8
fali <fali.randriamirindra@cima.ca>
2024-12-12 13:52:10 -0500

    host.docker.internal revert it all

--- maeve/apply-runtime-patches.sh
+++ maeve/apply-runtime-patches.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-export CSMS_SP1_URL="ws://172.17.0.1/ws/cp001"
-export CSMS_SP2_URL="wss://172.17.0.1/ws/cp001"
-export CSMS_SP3_URL="wss://172.17.0.1/ws/cp001"
+export CSMS_SP1_URL="ws://host.docker.internal/ws/cp001"
+export CSMS_SP2_URL="wss://host.docker.internal/ws/cp001"
+export CSMS_SP3_URL="wss://host.docker.internal/ws/cp001"
 
   if [[ "$DEMO_VERSION" =~ sp2 || "$DEMO_VERSION" =~ sp3 ]]; then
     echo "Patching the CSMS to enable EVerest organization"
